69b20e8ebb0f22b3222d4bf6dfdecf2bdef49b49
QuieType <quietype00@gmail.com>
2026-06-01 13:39:10 -0600

    tiring

--- _prestart.js
+++ _prestart.js
@@ -19,7 +19,7 @@ ig.ENTITY.Player.inject({
             if (this.attackResetTimer === 0) {
 				
 				if (sc.model.player.config.clazz == "SPHEROMANCER") {
-					this.setAttackCount(1);
+					this.setAttackCount(2);
 				}
 				
 				this.counterHere += 1;
@@ -34,3 +34,95 @@ ig.ENTITY.Player.inject({
         }
     }
 });
+ig.ACTION_STEP.DO_ACTION = ig.ActionStepBase.extend({
+    entity/*: ig.Event.GetEntity*/: 0,
+    action/*: ig.Action*/: null,
+    wait/*: boolean*/: false,
+    keepState/*: boolean*/: false,
+    _actionEntity/*: unknown*/: null,
+    _wm/*: ig.Config*/: new ig.Config({
+        attributes: {
+            entity: {
+                _type: 'Entity',
+                _info: 'Entity to move',
+                _visualize: true,
+                _context: 'Entity',
+            },
+            action: {
+                _type: 'Action',
+                _info: 'The action to perform',
+                _rec_visualize: ig.ACTION_STEP,
+            },
+            repeating: {
+                _type: 'Boolean',
+                _info: 'Repeat Action',
+            },
+            wait: {
+                _type: 'Boolean',
+                _info: 'Wait until action is finished',
+            },
+            keepState: {
+                _type: 'Boolean',
+                _info: "Don't reset entity state after action",
+            },
+            immediately: {
+                _type: 'Boolean',
+                _info: 'If true: execute action immediately not interrupting currently running action. Will only execute steps without wait duration.',
+            },
+        },
+        width: 500,
+    }),
+    init(settings/*: ig.EVENT_STEP.DO_ACTION.Settings*/)/*: DO_ACTION*/ {
+        assertContent(settings, 'entity', 'action')
+        this.entity = settings.entity
+        this.action = new ig.Action('[GENERIC]', settings.action, false, settings.repeating)
+        this.action.eventAction = true
+        this.wait = settings.wait || false
+        this.keepState = settings.keepState || false
+        this.immediately = settings.immediately || false
+    },
+    clearCached()/*: void*/ {
+        this.action && this.action.clearCached()
+    },
+    start(data/*?: unknown*/, eventCall/*?: ig.EventCall*/)/*: void*/ {
+        const c = ig.Event.getEntity(this.entity, eventCall)
+        data._actionEntity = c
+        if (this.immediately) {
+            c.stashAction(true)
+            c.setAction(this.action)
+            c.forceExecuteAction()
+            c.resumeStashedAction(true)
+        } else c && c.setAction(this.action, this.keepState)
+    },
+    run(data/*?: unknown*/)/*: boolean*/ {
+        return this.immediately || !data._actionEntity || !this.wait
+            ? true
+            : data._actionEntity.currentAction == this.action || (data._actionEntity.respawn && data._actionEntity.respawn.timer)
+              ? false
+              : true
+    },
+})
+ig.EVENT_STEP.WAIT_UNTIL_ON_GROUND = ig.EventStepBase.extend({
+    _wm/*: ig.Config*/: new ig.Config({
+        attributes: {
+            entity: {
+                _type: 'Entity',
+                _info: 'Entity to move',
+                _visualize: true,
+                _context: 'Entity',
+            },
+        },
+    }),
+    init(settings/*: ig.EVENT_STEP.WAIT_UNTIL_ON_GROUND.Settings*/)/*: WAIT_UNTIL_ON_GROUND*/ {
+        this.entity = settings.entity
+    },
+    start(data/*?: unknown*/, eventCall/*?: ig.EventCall*/)/*: void*/ {
+        const c = ig.Event.getEntity(this.entity, eventCall)
+        if (c) data._actionEntity = c
+    },
+    run(data/*?: unknown*/)/*: boolean*/ {
+        if (!data._actionEntity) return true
+        data = data._actionEntity
+        return data.coll.vel.z >= 0 && !data.coll.zGravityFactor ? true : data.coll.pos.z == data.coll.baseZPos
+    },
+})
